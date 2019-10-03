d30e324fc813e03bb948e8842aaaf4ad94864fe3
FedeLiborio <federico-l-@hotmail.com>
2019-10-02 21:39:10 -0300

    Modelo casi terminado - revisar

--- maxproductos/models.py
+++ maxproductos/models.py
@@ -1,3 +1,75 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    nombre = models.CharField(max_lenght=50)
+    apellido = models.CharField(max_length=50)
+    direccion = models.CharField(max_lenght=100)
+    telefono = models.BigIntegerField()
+    email = models.EmailField()
+    nombreUsuario = models.CharField(max_length=50)
+    contrasenia = models.CharField(max_length=50)
+
+class Proveedor(Usuario):
+    #horariosDeEntrega
+    descripcionNegocio = models.CharField(max_lenght=200)
+    calificacion = models.IntegerField
+
+class Cliente(Usuario):
+    #buscar sobre uno a uno e implementar
+    carro = models.ForeignKey(
+        'Carro',
+        on_delete=models.CASCADE,)
+
+class Administrador(Usuario):
+    pass
+
+class Producto(models.Model):
+    nombre = models.CharField(max_lenght=50)
+    tipo = models.ForeignKey(
+        'TipoProducto',
+        on_delete=models.CASCADE)
+    descripcion = models.CharField(max_lenght=200)
+    valor = models.DecimalField
+    proveedor = models.ForeignKey(
+        'Proveedor',
+        on_delete = models.CASCADE)
+    carro = models.ForeignKey(
+        'Carro',
+        on_delete=models.CASCADE)
+    pedido = models.ForeignKey(
+        'Pedido', 
+        on_delete = models.CASCADE)
+
+class TipoProducto(models.Model):
+    nombre = models.CharField(max_lenght=50)
+
+class Carro(models.Model):
+    #listaProductos
+    metodoDePago = models.ForeignKey(
+        'MetodoDePago',
+        on_delete=models.CASCADE)
+
+class MetodoDePago(models.Model):
+    nombre = models.CharField(max_length=50)
+
+class Pedido(models.Model):
+    #tiene una lista de productos
+    
+    confirmado = models.BooleanField()
+    #cuando confirmado = true, el pedido pasa a la lista de pedidos para entregar
+    
+    cliente = models.ForeignKey(
+        'Cliente',
+        on_delete = models.CASCADE)
+    fecha = models.DateField()
+    hora = models.TimeField()
+    proveedor = models.ForeignKey(
+        'Proveedor', 
+        on_delete=models.CASCADE)
+    direccion = models.CharField(max_lenght=100)
+    entregado = models.BooleanField()
+
+class Horario(models.Model):
+    horaInicio = models.IntegerField()
+    horaFinal = models.IntegerField()
+    dia = models.CharField(max_length=15)
